28fb0e8f13bcd4a17af650b99b23db85cfe9e824
yash raj <yashraj0207@outlook.com>
2024-09-07 13:27:16 +0530

    create a logger package

--- src/cmd/api/main.go
+++ src/cmd/api/main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"log"
-
+	"github.com/Real-Dev-Squad/reciprocal-backend/src/internal/logger"
 	"github.com/Real-Dev-Squad/reciprocal-backend/src/internal/server"
 )
 
 func main() {
 	server := server.NewServer()
 
-	log.Println("Server running on port 4000")
+	logger.Info("Server running on port 4000")
 	if err := server.ListenAndServe(); err != nil {
 		panic(err)
 	}
