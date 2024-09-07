d931a97d0a742dc501693079b9438b68068a3fe7
yash raj <yashraj0207@outlook.com>
2024-09-07 18:40:34 +0530

    setup sqlc

--- src/internal/server/routes.go
+++ src/internal/server/routes.go
@@ -3,6 +3,7 @@ package server
 import (
 	"net/http"
 
+	"github.com/Real-Dev-Squad/reciprocal-backend/src/internal/documents"
 	"github.com/Real-Dev-Squad/reciprocal-backend/src/internal/health"
 )
 
@@ -10,6 +11,7 @@ func (s *Server) RegisterRoutes() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	health.HealthRouteGroup(mux, s.db)
+	documents.DocumentsRouteGroup(mux, s.db)
 
 	return mux
 }
